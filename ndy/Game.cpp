79e816bfd9a825ac8ce22cb8a6875cd7c2525c00
Mingyu Kim <51695816+hse09021@users.noreply.github.com>
2023-04-17 23:10:21 +0900

    Update Game.cpp

--- ndy/Game.cpp
+++ ndy/Game.cpp
@@ -10,15 +10,15 @@ using namespace std;
 class Character
 {
     private:
-        char* name;    // 캐릭터 이름
-        int level;      // 캐릭터 레벨 
-        int power;      // 캐릭터 힘
-        int quickly;    // 캐릭터 민첩
-        int iq;         // 캐릭터 지능
-        int attack;    // 캐릭터 공경    
-        int shield;     // 캐릭터 방어력
-        int health;     // 캐릭터 체력
-        int mind;       // 캐릭터 정신력 
+        char* name;
+        int level;
+        int power;
+        int quickly;
+        int iq;
+        int attack;
+        int shield;
+        int health;
+        int mind;
 
     public:
 
@@ -33,18 +33,18 @@ class Character
             this->health = health;
             this->mind = mind;
         }
-        ~Character() {}  // 가상 Charactor 클래스 생성
+        ~Character() {}
         void Moshowinfo (char* weapon){
-            cout<<"캐릭터 이름 : "<<name<<endl;
-            cout<<"캐릭터 무기 : "<<weapon<<endl;
-            cout<<"캐릭터 레벨 : "<<level<<endl;
-            cout<<"캐릭터 힘 : "<<power<<endl;
-            cout<<"캐릭터 민첩 : "<<quickly<<endl;
-            cout<<"캐릭터 지능 : "<<iq<<endl;
-            cout<<"캐릭터 공격 : "<<attack<<endl;
-            cout<<"캐릭터 방어력 : "<<shield<<endl;
-            cout<<"캐릭터 체력 : "<<health<<endl;
-            cout<<"캐릭터 정신력 : "<<mind<<endl;
+            cout<<"Name : "<<name<<endl;
+            cout<<"Weapon : "<<weapon<<endl;
+            cout<<"Level : "<<level<<endl;
+            cout<<"Power : "<<power<<endl;
+            cout<<"Quickly : "<<quickly<<endl;
+            cout<<"IQ : "<<iq<<endl;
+            cout<<"Attack : "<<attack<<endl;
+            cout<<"Shield : "<<shield<<endl;
+            cout<<"Health : "<<health<<endl;
+            cout<<"Mind : "<<mind<<endl;
         }
         
 };
@@ -52,70 +52,67 @@ class Character
 class Warrior: public Character
 {
     private:
-        char* weapon = new char[MAX];     // 무기 
+        char* weapon = new char[MAX]; 
     public:
         Warrior(char* name, char* weapon) 
          : Character(name, 1, 100, 50, 20, 5, 3, 80,20)
         {
             this->weapon = weapon;
-            // strcpy(this->weapon, weapon);
-        }    // Warrior 디폴트 생성자 
+        } 
         void attack(){
-            cout<<this->weapon<<" 칼로 찔렀습니다."<<endl;
+            cout<<this->weapon<<" stabbed in knife."<<endl;
         }
         void showInfo(){
             Moshowinfo(weapon);
         }
         void move(){
-            cout<<"이동하였습니다."<<endl;
+            cout<<"moved."<<endl;
         }
-        ~Warrior() {}   // Warrior 생성자 삭제 
+        ~Warrior() {}
 };
 
 class Archer: public Character
 {
     private:
-        char* weapon;     // 무기
+        char* weapon;
     public:
         Archer(char* name, char* weapon) 
             : Character(name, 1, 50, 100, 20, 5, 3, 50,50)
         {
             this->weapon = weapon;
-            // strcpy(this->weapon, weapon);
-        }   // Archer 디폴트 생성자 
+        }
         void attack(){
-            cout<<this->weapon<<" 화살을 쐈습니다."<<endl;
+            cout<<this->weapon<<" shot an arrow."<<endl;
         }
         void showInfo(){
             Moshowinfo(weapon);
         }
         void move(){
-            cout<<"이동하였습니다."<<endl;
+            cout<<"moved."<<endl;
         }
-        ~Archer() {}   // Archer 생성자 삭제 
+        ~Archer() {}
 };
 
 class Sorcerer: public Character
 {
     private:
-        char* weapon;     // 무기
+        char* weapon;
     public:
         Sorcerer(char* name, char* weapon)
             : Character(name, 1, 20, 50, 100, 5, 3, 20,80)
         {
             this->weapon = weapon;
-            // strcpy(this->weapon, weapon);
-        }    // Sorcerer 디폴트 생성자 
+        }
         void attack(){
-            cout<<this->weapon<<"마법을 걸었습니다."<<endl;
+            cout<<this->weapon<<"used magic."<<endl;
         }
         void showInfo(){
             Moshowinfo(weapon);
         }
         void move(){
-            cout<<"이동하였습니다."<<endl;
+            cout<<"moved."<<endl;
         }
-        ~Sorcerer() {}   // Sorcerer 생성자 삭제 
+        ~Sorcerer() {}
 };
 
 class Bag
@@ -133,9 +130,9 @@ class Bag
                 return "False";
         }
         void Size(int size){
-            cout<<"현재 bag의 사이즈는 "<<size<<" 입니다."<<endl;
+            cout<<"Currently size of the bag is "<<size<<endl;
         }
-        ~Bag() {};   // Bag 생성자 삭제 
+        ~Bag() {};
 };
 
 int main()
@@ -144,28 +141,28 @@ int main()
     char* weapon = new char[MAX];
     Bag bg;
     bg.Size(0);
-    cout<<"IsEmpty 값은 "<<bg.IsEmpty(0)<<" 입니다."<<endl;
-    cout<<"Warrior 캐릭터 이름을 입력하세요 : ";
+    cout<<"IsEmpty? "<<bg.IsEmpty(0)<<endl;
+    cout<<"Warrior's Name? : ";
     cin>>name;
-    cout<<"무기 이름을 입력하세요 : ";
+    cout<<"Weapon : ";
     cin>>weapon;
     Warrior wa(name, weapon);
     wa.attack();
     wa.showInfo();
     wa.move();
 
-    cout<<"Archer 캐릭터 이름을 입력하세요 : ";
+    cout<<"Archer's Name? : ";
     cin>>name;
-    cout<<"무기 이름을 입력하세요 : ";
+    cout<<"Weapon : ";
     cin>>weapon;
     Archer Ar(name, weapon);
     Ar.attack();
     Ar.showInfo();
     Ar.move();
 
-    cout<<"Sorcerer 캐릭터 이름을 입력하세요 : ";
+    cout<<"Sorcerer's Name? : ";
     cin>>name;
-    cout<<"무기 이름을 입력하세요 : ";
+    cout<<"Weapon : ";
     cin>>weapon;
     Sorcerer So(name, weapon);
     So.attack();
